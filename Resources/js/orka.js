0b2e088dc5404b810c96b16f1bdecb02a3fa94cb
Rob Griffiths <rob@bytespider.eu>
2011-02-03 11:49:39 +0000

    added simple templating

--- Resources/js/orka.js
+++ Resources/js/orka.js
@@ -1,4 +1,18 @@
 (function (window) {
 
-
+	// pick up template from DOM
+	var templates = {};
+	var DOMtemplates = document.querySelectorAll('[template-name]');
+	for(var i = 0, len = DOMtemplates.length; i < len; ++i) {
+		var name = DOMtemplates[i].getAttribute('template-name');
+		
+		var str = DOMtemplates[i].innerHTML(/[\r\t\n]+/g, " ").replace("{%","'+").replace("%}","+'");
+		
+		templates[name] = new Function("item", "var tpl = document.createElement('div'); tpl.innerHTML = '" + str + "'; return tpl.firstChild;");
+		
+		DOMtemplates[i].parentNode.removeChild(DOMtemplates[i]);
+	}
+	DOMtemplates = null;
+	delete DOMtemplates;
+	
 })(this);
